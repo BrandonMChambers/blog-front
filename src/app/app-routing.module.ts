b4bd35f895b07b3294345ad2cf25837ad3f2b8a4
Brandon Chambers <brandonmchambers36@gmail.com>
2020-01-10 09:48:37 -0500

    add BlogEntryViewComponent to app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { BlogComponent } from './component/blog/blog/blog.component';
 import { BlogEntry } from './model/blogEntry';
 import { BlogFormComponent } from './component/blog-form/blog-form/blog-form.component';
 import { EntryFormComponent } from './component/entry-form/entry-form/entry-form.component';
+import { BlogEntryViewComponent } from './component/blog-entry-view/blog-entry-view/blog-entry-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'blog/:blogId', component: BlogComponent },
-  { path: 'blog/:blogId/entry/:entryId', component: BlogEntry },
+  { path: 'blog/:blogId/entry/:entryId', component: BlogEntryViewComponent },
   { path: 'create/blog', component: BlogFormComponent },
   { path: 'blog/:blogId/create', component: EntryFormComponent}
 ];
